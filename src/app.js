168ed4dca41760eda9860c0bcd7aaa7043f851e5
erjanmx <erjanmx@gmail.com>
2017-10-10 13:29:17 +0300

    Add image files handler

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 /*jshint esversion: 6 */
+import 'whatwg-fetch'
 import Vue from 'vue';
 
 require('../assets/app.scss');
 
+// using proxy because n1 servers do not allow cross-origin requests
+var file_api_url = 'https://cors-anywhere.herokuapp.com/https://files.namba1.co?token=';
 var socket = require('socket.io-client')('http://localhost:3000');
 
-socket.on('stream', function(data){
+function parseBlob(response) {
+  return response.blob();
+}
+
+socket.on('stream', function(data) {
   app.bot_status = 'connected';
 
   console.log(data);
   if (data.post_params.content) {
+    var msg_id = Date.now();
+
+    if (data.post_params.type != 'text/plain') {
+      fetch(file_api_url + data.post_params.content).then(parseBlob).then(function (blob) {
+        var img = document.getElementById(msg_id);
+        img.src = URL.createObjectURL(blob);
+      });
+    }
+
     app.messages.push({
+      'msg_id': msg_id,
+      'type': data.post_params.type,
       'from_bot': true,
       'content': data.post_params.content,
     });
@@ -31,7 +49,9 @@ var app = new Vue({
         }
 
         this.messages.push({
+            'msg_id': Date.now(),
             'from_bot': false,
+            'type': 'text/plain',
             'content': this.input,
         });
 
