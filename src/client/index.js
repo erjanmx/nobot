cb6046f75b2d292a4683fa97831571cc7ab1ee86
erjanmx <erjanmx@gmail.com>
2017-11-24 21:03:42 +0300

    Remove vue production tip

--- src/client/index.js
+++ src/client/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueSocket from 'vue-socket.io'
 
-Vue.config.productionTip = false
-
 const port = require('../../settings.json').port
 const socket = require('socket.io-client')(`http://127.0.0.1:${port}`)
 
