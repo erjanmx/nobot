b85c433eaef129043b3847f025b987241f98dd52
erjanmx <erjanmx@gmail.com>
2017-11-24 10:40:54 +0300

    Style changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  entry: "./src/client/index.js",
-  output: {
-    filename: "public/js/bundle.js"
-  },
+    entry: './src/client/index.js',
+    output: {
+        filename: 'public/js/bundle.js'
+    },
 
-  devServer: {
-    inline: true
-  },
+    devServer: {
+        inline: true
+    },
 
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.js'
-    }
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          hotReload: true // disables Hot Reload
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
         }
-      }
-    ]
-  }
-};
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    hotReload: true
+                }
+            }
+        ]
+    }
+}
